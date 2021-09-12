6fe6da4dd8c6a69233d698f3501b8d2ced945e65
galczynskak <galczynska.karolina@gmail.com>
2021-09-12 14:34:43 +0200

    projekt 3 init

--- main.py
+++ main.py
@@ -1,58 +1,18 @@
-from cube import *
 from image import *
-from matrix import *
-from painter import paint
 
-zoom_step = 50.
-window_width, window_height = 1000, 1000
-observer = [0, 0, -501.]
+window_size = (1000, 1000)
 screenshot_index = 1
 clear_directory('screenshots')
 
-positions, dimensions = load_cubes_from_file("cubes.txt")
-cubes = []
-for pos, dim in zip(positions, dimensions):
-    cubes.append(create_cube(pos, dim))
-
-window = pygame.display.set_mode((window_width, window_height))
+window = pygame.display.set_mode(window_size)
 pygame.init()
-pygame.display.set_caption("Visual Camera")
+pygame.display.set_caption("phong model")
 
-run = True
+run=True
 while run:
     for event in pygame.event.get():
         run = False if event.type == pygame.QUIT else True
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_a]:
-            cubes = translate(cubes, 'a')
-        if keys[pygame.K_d]:
-            cubes = translate(cubes, 'd')
-        if keys[pygame.K_s]:
-            cubes = translate(cubes, 's')
-        if keys[pygame.K_w]:
-            cubes = translate(cubes, 'w')
-        if keys[pygame.K_f]:
-            cubes = translate(cubes, 'f')
-        if keys[pygame.K_r]:
-            cubes = translate(cubes, 'r')
-
-        if keys[pygame.K_j]:
-            cubes = rotate(cubes, 'k')
-        if keys[pygame.K_l]:
-            cubes = rotate(cubes, 'i')
-        if keys[pygame.K_k]:
-            cubes = rotate(cubes, 'j')
-        if keys[pygame.K_i]:
-            cubes = rotate(cubes, 'l')
-        if keys[pygame.K_h]:
-            cubes = rotate(cubes, 'h')
-        if keys[pygame.K_y]:
-            cubes = rotate(cubes, 'y')
-
-        if keys[pygame.K_UP]:
-            observer[2] += zoom_step
-        if keys[pygame.K_DOWN]:
-            observer[2] -= zoom_step if observer[2] - zoom_step >= 0 else 1
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
@@ -64,6 +24,5 @@ while run:
             raise SystemExit
 
     window.fill((0, 0, 0))
-    paint(cubes, window, observer, window_height, window_width)
 
     pygame.display.update()
